9a982501f05e6243e9cdf7d7be1db04ff811a286
Luis Alberto Canizares (Alberto) <39811447+canizarl@users.noreply.github.com>
2022-11-22 17:34:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['sunpy', 'astropy', 'matplotlib', 'numpy', 'sys', 'datetime'],
+    install_requires=['sunpy', 'astropy'],
     keywords=['python', 'solar', 'nasa', 'esa', 'spacecraft', 'coordinates', 'sun', 'corona', 'parker', 'solar orbiter'],
     classifiers=[
         "Development Status :: 1 - Planning",
